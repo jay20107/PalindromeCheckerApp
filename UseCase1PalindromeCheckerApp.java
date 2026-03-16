02ddb7d4ee8e284e8c305c4dc7e5b36e7392dca1
jay20107 <jay.solanki2007@gmail.com>
2026-03-16 08:56:51 +0530

    Added UseCase1 Palindrome Checker App

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("      Palindrome Checker App      ");
+        System.out.println("==================================");
+        System.out.println("Version: 1.0");
+        System.out.println("Welcome to the Palindrome Checker Application");
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println("Application started successfully.");
+    }
+}
