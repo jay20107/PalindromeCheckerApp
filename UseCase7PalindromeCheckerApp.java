562da7bc13588e540b1947e5227fe810a12f0dd0
jay20107 <jay.solanki2007@gmail.com>
2026-03-16 09:50:36 +0530

    Added UseCase7 Deque based Palindrome Checker

---
+++ UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
