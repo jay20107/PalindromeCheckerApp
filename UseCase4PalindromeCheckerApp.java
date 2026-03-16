44500fad19b5a318952a3bb6bff516be2cd0012d
jay20107 <jay.solanki2007@gmail.com>
2026-03-16 09:33:51 +0530

    Added UseCase4 Character Array Palindrome Check

---
+++ UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,27 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "radar";
+        char[] characters = word.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
